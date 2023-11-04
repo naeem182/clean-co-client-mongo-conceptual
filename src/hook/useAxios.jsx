f22232bf54e39c8e3fc9b2efee69741614cc5ba9
naeem <naeemislamjr182@gmail.com>
2023-11-05 04:08:23 +0600

    useaxios

--- src/hook/useAxios.jsx
+++ src/hook/useAxios.jsx
@@ -4,15 +4,11 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'http://localhost:5000/api/v1',
-
+    withCredentials: true,
 });
 
 const useAxios = () => {
-    return (
-        <div>
-
-        </div>
-    )
+    return instance;
 }
 
 export default useAxios
